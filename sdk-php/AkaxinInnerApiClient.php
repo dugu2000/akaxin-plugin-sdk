fcaba81be8220b9f50e77e0337c7e0689e988df0
sisishiliu <4416@cunmou.com>
2018-05-31 16:54:18 +0800

    add AkaxinInnerApiClient.php

---
+++ sdk-php/AkaxinInnerApiClient.php
@@ -0,0 +1,116 @@
+<?php
+
+use \Google\Protobuf\Internal\Message;
+
+
+
+// 加载Google Protobuf Library
+// ==========================
+require_once(__DIR__ ."/vendor/autoload.php");
+
+// 加载AkaxinProtoSDK
+// ==================
+// Akaxin\Proto 有很多，在sdk-php文件夹下，具体接口使用哪一个，请参考接口文档。
+function autoloadForAkaxinProtoSDK($className) {
+    $className = str_replace("\\", "/", $className);
+    require_once(__DIR__ . "/{$className}.php");
+}
+spl_autoload_register("autoloadForAkaxinProtoSDK");
+
+
+
+
+class AkaxinInnerApiClient {
+
+    private $apiHost = "127.0.0.1";
+    private $apiPort = 8280;
+    private $authkey = "";
+    private $pluginId = -1;
+
+    private $sessionSiteUserId = "";
+
+    private $lastErrorCode = "";
+    private $lastErrorInfo = "";
+
+    /**
+     * 设置InnerAPI服务器地址、authkey
+     */
+    public function __construct($apiHost, $apiPort, $pluginId, $authkey) {
+        $this->apiHost = $apiHost;
+        $this->apiPort = $apiPort;
+        $this->pluginId = $pluginId;
+        $this->authkey = $authkey;
+    }
+
+    public function setSessionSiteUserId($siteUserId) {
+        $this->sessionSiteUserId = $siteUserId;
+    }
+
+    public function errorCode() {
+        return $this->lastErrorCode;
+    }
+
+    public function errorInfo() {
+        return $this->lastErrorInfo;
+    }
+
+    public function request($actionName, Message $requestMessage) {
+        $requestPackage = new Akaxin\Proto\Core\ProxyPluginPackage();
+        $data = $requestMessage->serializeToString();
+        $data = base64_encode($data); //千万不要忘了这一步！
+        $requestPackage->setData($data);
+        $requestPackage->setPluginHeader($this->makeRequestHeader());
+
+        $responsePackage = $this->curlRequest($actionName, $requestPackage);
+        if (false === $response) {
+            return false;
+        }
+
+        $errorInfo = $responsePackage->getErrorInfo();
+        $this->lastErrorCode = $errorInfo->getCode();
+        $this->lastErrorInfo = $errorInfo->getInfo();
+
+        // 业务逻辑，开始解析Response
+        // ===============
+        return base64_decode($responsePackage->getData());
+    }
+
+    private function curlRequest($actionName, Message $requestPackage) {
+        $postData = openssl_encrypt($requestPackage->serializeToString() , "AES-128-ECB", $this->authkey, OPENSSL_RAW_DATA);
+
+        $actionName = trim($actionName, "/\\ ");
+        $url = "http://{$this->apiHost}:{$this->apiPort}/{$actionName}";
+        $ch = curl_init("http://{$this->apiHost}:{$this->apiPort}/{$actionName}");
+        $httpHeader = array(
+            "site-plugin-id: {$this->pluginId}", // 替换为自己的扩展ID
+        );
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $httpHeader);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $responseInBinary = curl_exec($ch); // curl可能会出错，要容错。
+
+        if (false === $responseInBinary) {
+            $this->lastErrorCode = "curl_errno: " . curl_errno($ch);
+            $this->lastErrorInfo = "curl_info: " . curl_error($ch);
+            trigger_error("curl_exec errors {$url} " . curl_error($ch));
+        }
+
+        $responseData = openssl_decrypt($responseInBinary, "AES-128-ECB", $this->authkey, OPENSSL_RAW_DATA); // 解密可能失败
+        $responsePackage = new Akaxin\Proto\Core\ProxyPluginPackage();
+        $responsePackage->mergeFromString($responseData);
+        return $responsePackage;
+    }
+
+    private function makeRequestHeader() {
+        $requestPakcageHeader = array(
+            Akaxin\Proto\Core\PluginHeaderKey::PLUGIN_TIMESTAMP => time()*1000, // 安全作用
+        );
+
+        if (!empty($this->sessionSiteUserId)) {
+            $key = Akaxin\Proto\Core\PluginHeaderKey::CLIENT_SITE_USER_ID;
+            $requestPakcageHeader[$key] = $this->sessionSiteUserId;
+        }
+        return $requestPakcageHeader;
+    }
+}
--- sdk-php/demo-php.php
+++ sdk-php/demo-php.php
@@ -18,24 +18,7 @@
 //
 
 // 体验前，先remove掉这一句，防止被别人访问。
-die("https://github.com/akaxincom/openzaly");
-
-
-
-// 加载Google Protobuf Library
-// ==========================
-require_once(__DIR__ ."/vendor/autoload.php");
-
-
-
-// 加载AkaxinProtoSDK
-// ==================
-// Akaxin\Proto 有很多，在sdk-php文件夹下，具体接口使用哪一个，请参考接口文档。
-function autoloadForAkaxinProtoSDK($className) {
-    $className = str_replace("\\", "/", $className);
-    require_once(__DIR__ . "/{$className}.php");
-}
-spl_autoload_register("autoloadForAkaxinProtoSDK");
+// die("https://github.com/akaxincom/openzaly");
 
 
 // 初始化设置
@@ -43,70 +26,30 @@ spl_autoload_register("autoloadForAkaxinProtoSDK");
 // 本源码以获取用户Profile接口为例
 $innerApiHost = "127.0.0.1";        // 对应启动服务器时的 -Dhttp.address 参数
 $innerApiPort = 8280;               // 对应启动服务器时的 -Dhttp.port 参数
-$action_name = "hai/user/profile";  // 请查阅相关接口文档
 $pluginAuthKey = "XxlgqWBc6N4fMWZF";// 管理平台->扩展列表，点击相应的扩展获取。
-$siteUserIdForQuery = "c15c4e03-ab76-4d1e-bf89-dc8699ae02ca";
+$pluginId = 3;
+
+
+require_once(__DIR__ . "/AkaxinInnerApiClient.php");
+$akaxinApiClient = new AkaxinInnerApiClient($innerApiHost, $innerApiPort, $pluginId, $pluginAuthKey);
 
 
 // 构造Request
 // ==========
-$bodyData = new Akaxin\Proto\Plugin\HaiUserProfileRequest();
-$bodyData->setSiteUserId($siteUserIdForQuery);
+$siteUserIdForQuery = "c15c4e03-ab76-4d1e-bf89-dc8699ae02ca";
+$requestMessage = new Akaxin\Proto\Plugin\HaiUserProfileRequest();
+$requestMessage->setSiteUserId($siteUserIdForQuery);
 
-// 底层网络->数据包封装开始
-// ===================
-// 这部分逻辑是通用的，应该抽离成单独的方法
-$requestPackage = new Akaxin\Proto\Core\ProxyPluginPackage();
-$data = $bodyData->serializeToString();
-$data = base64_encode($data); //千万不要忘了这一步！
-$requestPackage->setData($data);
 
-// 底层网络->设置ProxyPluginPackage.header
-// ============================
-$requestPakcageHeader = array(
-    Akaxin\Proto\Core\PluginHeaderKey::PLUGIN_TIMESTAMP => time()*1000, // 安全作用
-    Akaxin\Proto\Core\PluginHeaderKey::CLIENT_SITE_USER_ID => "daaaaaa-ab76-bbbb-bf89-dc8699aecccc", // 代表当前操作这个行为的用户
+// 执行请求
+// =======
+$responseData = $akaxinApiClient->request("hai/user/profile", $requestMessage);
 
-);
-$requestPackage->setPluginHeader($requestPakcageHeader);
 
-// 底层网络->加密数据包
+// 处理返回逻辑
 // ========
-$requestPackageSerialized = $requestPackage->serializeToString();
-
-// 底层网络->扩展需要支持向量。
-$postData = openssl_encrypt($requestPackageSerialized , "AES-128-ECB", $pluginAuthKey, OPENSSL_RAW_DATA);
-
-// 底层网络->执行网络请求
-// ==========
-$ch = curl_init("http://{$innerApiHost}:{$innerApiPort}/{$action_name}");
-$httpHeader = array(
-    "site-plugin-id: 3", // 替换为自己的扩展ID
-    "X-Forwarded-For: 127.0.0.1" // 这个接口即将被废弃
-);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $httpHeader);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$responseInBinary = curl_exec($ch); // curl可能会出错，要容错。
-
-
-// 底层网络->开始解析Response
-// ===============
-$responseData = openssl_decrypt($responseInBinary, "AES-128-ECB", $pluginAuthKey, OPENSSL_RAW_DATA); // 解密可能失败
-$responsePackage = new Akaxin\Proto\Core\ProxyPluginPackage();
-$responsePackage->mergeFromString($responseData);
-
-$errorInfo = $responsePackage->getErrorInfo();
-var_dump("errorCode: " . $errorInfo->getCode());
-var_dump("errorInfo: " . $errorInfo->getInfo());
-
-// 业务逻辑，开始解析Response
-// ===============
 $userProfileResponse = new Akaxin\Proto\Plugin\HaiUserProfileResponse();
-$data = base64_decode($responsePackage->getData());
-$userProfileResponse->mergeFromString($data);
+$userProfileResponse->mergeFromString($responseData);
 $userProfile = $userProfileResponse->getUserProfile();
 
-// 查看请求返回的值
 var_dump($userProfile->getSiteUserId(), $userProfile->getUserName());
