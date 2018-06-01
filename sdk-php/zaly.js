7057fd4af5b1ed8e23ac529f1f1d236685812faf
childeYin <zhangjun516@126.com>
2018-06-01 19:55:31 +0800

    修改zaly js, add zaly js md

--- sdk-php/zaly.js
+++ sdk-php/zaly.js
@@ -1,10 +1,11 @@
-(function ($) {
+
+var Zaly = {
     /**
      * 获取机器类型
      * @author zhangjun
      * @returns {string}
      */
-    var osType = function getOsType() {
+    getOsType : function getOsType() {
         var u = navigator.userAgent;
         if (u.indexOf('Android') > -1 || u.indexOf('Linux') > -1) {
             return 'Android';
@@ -13,132 +14,107 @@
         } else {
             return 'PC';
         }
-    };
+    },
 
-    var toast = function toast(msgStr) {
+    /**
+     * 请求数据
+     *
+     * @param reqUri String  请求地址
+     * @param params mix 请求参数
+     * @param callbackName String  js执行完成之后，回调方法的名字
+     */
+    reqData : function reqData(reqUri, params, callbackName) {
+        if (this.osType == 'Android') {
+            Android.requestPost(reqUri, params, callbackName);
+        } else if (this.osType == 'IOS') {
+            ios_requestPost(reqUri, params, callbackName);
+        } else {
+            this.tip('暂时不支持该设备');
+        }
+    },
+
+    /**
+     * 加载渲染页面
+     *
+     * @param reqUri String 请求地址
+     * @param params mix 请求参数
+     */
+    reqPage : function reqPage(reqUri, params) {
+        if (this.osType == 'Android') {
+            Android.requestPage(reqUri, params)
+        } else if (this.osType == 'IOS') {
+            ios_requestPage(reqUri, params);
+        } else {
+            this.tip('暂时不支持该设备');
+        }
+    },
+
+    /**
+     *
+     * 图片上传, 直接加载渲染页面
+     *
+     * @param callback String js执行完成之后，回调方法的名字
+     */
+    reqImageUpload : function reqImageUpload(callback) {
+        if (this.osType == 'Android') {
+            Android.imageUpload(callback);
+        } else if (this.osType == 'IOS') {
+            ios_imageUpload(callback);
+        } else {
+            this.tip('暂时不支持该设备');
+        }
+    },
+
+    /**
+     * 客户端下载图片
+     *
+     * @param imageId String 图片id
+     * @param callback String js执行完成之后，回调方法的名字
+     */
+    reqImageDownload : function reqImageDownload(imageId, callback) {
         if (osType == 'Android') {
-            Android.showToast(msgStr);
+            Android.imageDownload(imageId, callback);
         } else if (osType == 'IOS') {
-            $.toast(msgStr);
+            ios_imageDownload(imageId, callback);
         } else {
-            console.log(msgStr);
+            this.tip('暂时不支持该设备');
         }
-    };
+    },
 
-    var zalyMethods = {
-        /**
-         * 请求数据
-         *
-         * @param reqUri String  请求地址
-         * @param params mix 请求参数
-         * @param callbackName String  js执行完成之后，回调方法的名字
-         */
-        reqUrl : function reqUrl(reqUri, params, callbackName) {
-            if (osType == 'Android') {
-                Android.requestPost(reqUri, params, callbackName);
-            } else if (osType == 'IOS') {
-                ios_requestPost(reqUri, params, callbackName);
-            } else {
-                toast('暂时不支持该设备');
-            }
-        },
-
-        /**
-         * 加载渲染页面
-         *
-         * @param reqUri String 请求地址
-         * @param params mix 请求参数
-         */
-        reqHtml : function reqHtml(reqUri, params) {
-            if (osType == 'Android') {
-                Android.requestPage(reqUri, params)
-            } else if (osType == 'IOS') {
-                ios_requestPage(reqUri, params);
-            } else {
-                toast('暂时不支持该设备');
-            }
-        },
-
-        /**
-         *
-         * 图片上传, 直接加载渲染页面
-         *
-         * @param callback String js执行完成之后，回调方法的名字
-         */
-        reqImageUpload : function reqImageUpload(callback) {
-            var type = $.extend(getOsType)
-            if (type == 'Android') {
-                Android.imageUpload(callback);
-            } else if (type == 'IOS') {
-                ios_imageUpload(callback);
-            } else {
-                toast('暂时不支持该设备');
-            }
-        },
-
-        /**
-         * 客户端下载图片
-         *
-         * @param imageid String 图片id
-         * @param callback String js执行完成之后，回调方法的名字
-         */
-        reqImageDownload : function reqImageDownload(imageid, callback) {
-            var type = $.extend(getOsType)
-            if (type == 'Android') {
-                Android.imageDownload(imageid, callback);
-            } else if (type == 'IOS') {
-                ios_imageDownload(imageid, callback);
-            } else {
-                toast('暂时不支持该设备');
-            }
-        },
-
-
-        /**
-         * 客户端toast信息，用来提示用户
-         *
-         * @param msgStr
-         */
-        tip :  function tip(msgStr) {
-            if (osType == 'Android') {
-                Android.showToast(msgStr);
-            } else if (osType == 'IOS') {
-                $.toast(msgStr);
-            } else {
-                toast('暂时不支持该设备');
-            }
-        },
-
-        /**
-         * 刷新当前的页面
-         */
-        refreshCurrentPage : function refreshCurrentPage() {
-            if (osType == 'Android') {
-                Android.refreshCurrentPage();
-            }
-        },
-
-        /**
-         * 扩展跳转地址
-         *
-         * @param url String 跳转地址
-         */
-        gotoPage : function gotoPage(url) {
-            if (osType == 'Android') {
-                Android.gotoPage(url);
-            } else if (osType == 'IOS') {
-                ios_gotoPage(url);
-            }
-        },
-    };
-
-    $.fn.zaly = function(method) {
-        if(zalyMethods[method]) {
-            return zalyMethods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-        } else if ( typeof method === 'object' || ! method ) {
-            return zalyMethods.init.apply( this, arguments );
+    /**
+     * 客户端toast信息，用来提示用户
+     *
+     * @param strTipMsg
+     */
+    tip : function tip(strTipMsg) {
+        if (this.osType == 'Android') {
+            Android.showToast(strTipMsg);
+        } else if (this.osType == 'IOS') {
+            $.toast(strTipMsg);
         } else {
-            $.error( 'Method ' +  method + ' does not exist on jQuery.zaly' );
+            console.log(strTipMsg);
         }
-    }
-})(jQuery);
+    },
+
+    /**
+     * 刷新当前的页面
+     */
+    refreshCurrentPage : function refreshCurrentPage() {
+        if (this.osType == 'Android') {
+            Android.refreshCurrentPage();
+        }
+    },
+
+    /**
+     * 扩展跳转地址
+     *
+     * @param url String 跳转地址
+     */
+    gotoPage : function gotoPage(gotoUrl) {
+        if (this.osType == 'Android') {
+            Android.gotoPage(gotoUrl);
+        } else if (this.osType == 'IOS') {
+            ios_gotoPage(gotoUrl);
+        }
+    },
+}
---
+++ sdk-php/zalyJs.md
@@ -0,0 +1,306 @@
+## ZalyJs
+=====
+
+### 基本引入
+=====
+>
+>  引用 Zaly Js
+>
+
+
+```
+<script type="text/javascript" src="./zaly.js"></script>
+
+```
+
+### Zaly Js提供的方法
+=====
+
+#### 1. getOsType
+=====
+>  
+> 获取机器类型
+> 
+> 返回类型 : String 
+> 
+> 返回值分别是 : Android, IOS, PC
+
+```
+var osType = Zaly.osType();
+console.log(osType);
+```
+
+
+#### 2. reqData 请求数据
+=====
+
+
+```
+Zaly.reqData(reqUri, params, callbackName);
+```
+
+>  
+> 请求参数
+
+<table>
+<tr>
+<td>名字</td>
+<td>类型</td>
+<td>说明</td>
+</tr>
+<tr>
+<td> reqUri </td>
+<td> String </td>
+<td> 请求地址</td>
+</tr>
+<tr>
+<td> params </td>
+<td> mix </td>
+<td> 请求参数(建议json，或者序列化的数据)</td>
+</tr>
+<tr>
+<td> callbackName </td>
+<td> String </td>
+<td> js执行完成之后，回调方法的名字， 回调方法中，客户端会原封不动的返回server端的返回值。</td>
+</tr>
+</table>
+
+
+
+####3. reqPage 加载渲染页面
+=====
+
+
+```
+Zaly.reqUrl(reqUri, params);
+```
+
+>  
+> 请求参数
+
+<table>
+<tr>
+<td>名字</td>
+<td>类型</td>
+<td>说明</td>
+</tr>
+<tr>
+<td> reqUri </td>
+<td> String </td>
+<td> 请求地址</td>
+</tr>
+<tr>
+<td> params </td>
+<td> mix </td>
+<td> 请求参数(建议json，或者序列化的数据)</td>
+</tr>
+</table>
+
+
+
+#### 4. reqImageUpload 图片上传
+=====
+
+```
+Zaly.reqImageUpload(callbackName);
+
+```
+
+>  
+> 请求参数
+
+<table>
+<tr>
+<td>名字</td>
+<td>类型</td>
+<td>说明</td>
+</tr>
+
+<tr>
+<td> callback </td>
+<td> String </td>
+<td> js执行完成之后，回调方法的名字</td>
+</tr>
+</table>
+
+
+##### 回调方法中：
+
+```
+假如回调方法叫做： imageUpload
+
+则回调方法中，写为
+
+	function imageUpload(isUpload, imageId, imageSrcPath){
+	
+	}
+```
+
+>  
+> 返回参数
+
+<table>
+<tr>
+<td>名字</td>
+<td>类型</td>
+<td>说明</td>
+</tr>
+
+<tr>
+<td> isUpload </td>
+<td> Boolean </td>
+<td> 是否上传成功，1 成功， 0 失败</td>
+</tr>
+<tr>
+<td> imageId </td>
+<td> String </td>
+<td> 图片的id </td>
+</tr>
+<tr>
+<td> imageSrcPath </td>
+<td> String </td>
+<td> 图片的地址<br/>  Android 使用方式是：
+src = "http://akaxin/img' + imageSrcPath + '"
+<br/>
+IOS 使用方式是： src="' + imageSrcPath + '"</td>
+</tr>
+</table>
+
+
+
+#### 5. reqImageDownload 图片下载
+=====
+
+
+```
+Zaly.reqImageDownload(callbackName);
+
+```
+>  
+> 请求参数
+
+<table>
+<tr>
+<td>名字</td>
+<td>类型</td>
+<td>说明</td>
+</tr>
+
+<tr>
+<td> imageId </td>
+<td> String </td>
+<td> 图片id </td>
+</tr>
+<tr>
+<td> callback </td>
+<td> String </td>
+<td> js执行完成之后，回调方法的名字</td>
+</tr>
+</table>
+
+
+#####  回调方法中的处理：
+```
+假如回调方法叫做： imageDownload
+
+则回调方法中，写为
+
+	function imageDownload(isDownload, imageId, imageSrcPath){
+	
+	}
+```
+
+>  
+> 回调方法中，参数
+
+
+<table>
+<tr>
+<td>名字</td>
+<td>类型</td>
+<td>说明</td>
+</tr>
+
+<tr>
+<td> isDownload </td>
+<td> Boolean </td>
+<td> 是否下载成功，1 成功， 0 失败</td>
+</tr>
+<tr>
+<td> imageId </td>
+<td> String </td>
+<td> 图片的id </td>
+</tr>
+<tr>
+<td> imageSrcPath </td>
+<td> String </td>
+<td> 图片的地址<br/>  Android 使用方式是：
+src = "http://akaxin/img' + imageSrcPath + '"
+<br/>
+IOS 使用方式是： src="' + imageSrcPath + '"
+</td>
+</tr>
+</table>
+
+
+
+#### 6. tip 客户端toast信息，用来提示用户
+=====
+
+```
+Zaly.tip(strTipMsg);
+```
+
+>  
+> 请求参数
+
+<table>
+<tr>
+<td>名字</td>
+<td>类型</td>
+<td>说明</td>
+</tr>
+<tr>
+<td> strTipMsg </td>
+<td> String </td>
+<td>提示信息</td>
+</tr>
+</table>
+
+
+
+
+#### 7. refreshCurrentPage 刷新当前页面
+=====
+
+```
+Zaly.refreshCurrentPage();
+```
+* 目前只支持安卓客户端
+
+
+#### 8. gotoPage 扩展跳转地址
+=====
+
+```
+Zaly.tip(strTipMsg);
+```
+
+>  
+> 请求参数
+
+<table>
+<tr>
+<td>名字</td>
+<td>类型</td>
+<td>说明</td>
+</tr>
+<tr>
+<td> gotoUrl </td>
+<td> String </td>
+<td>跳转地址， 目前仅支持zaly|zalys跳转</td>
+</tr>
+</table>
+
+
+
