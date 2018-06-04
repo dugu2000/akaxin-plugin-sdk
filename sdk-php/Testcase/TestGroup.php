01e4519cdfec5b6ce0fd4abcb388798b1c44b0ab
sisishiliu <4416@cunmou.com>
2018-06-04 14:25:15 +0800

    add TestUser.php

---
+++ sdk-php/Testcase/TestGroup.php
@@ -0,0 +1,37 @@
+<?php
+// declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class HaiFriendTest extends TestCase
+{
+
+    public function testHaiFriendAddRequest(): void
+    {
+
+        $client = getApiClient();
+
+        $request = new Akaxin\Proto\Plugin\HaiFriendAddRequest();
+        $request->setSiteUserId(getSiteUserIdForTest());
+        $request->setFriendSiteUserId(getFriendUserIdForTest());
+
+        $responseData = $client->request("hai/friend/add", $request);
+
+        $response = new Akaxin\Proto\Plugin\HaiFriendAddResponse();
+        $response->mergeFromString($responseData);
+
+        $this->assertInstanceOf(
+            Akaxin\Proto\Plugin\HaiFriendAddResponse::class,
+            $response
+        );
+
+        $this->assertEquals(
+            $client->errorCode(),
+            ERROR_CODE_SUCCESS
+        );
+
+        $this->assertEmpty($client->errorInfo());
+
+    }
+
+}
---
+++ sdk-php/Testcase/TestUser.php
@@ -0,0 +1,21 @@
+<?php
+// declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class HaiUserTest extends TestCase
+{
+
+    public function testHaiFriendAddRequest(): void
+    {
+
+        ////
+
+    }
+
+
+    public function testHapixxxxxx() {
+        /////
+    }
+
+}
